94206c3169eee136219d9f724f7a240512fa287f
Illex <Illex@DESKTOP-8S5DVGP>
2021-11-17 14:44:46 -0800

    added pixi cdn, got basic pixi calls working

--- WebBase/wwwroot/js/site.js
+++ WebBase/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function setScreen() {
+
+    const app = new PIXI.Application({
+        width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+    });
+    document.getElementById("availability-table").appendChild(app.view);
+    //document.body.appendChild(app.view);
+}
