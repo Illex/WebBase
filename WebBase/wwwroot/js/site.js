9250f8c55d65710621384510deb056562c57c1c7
Illex <Illex@DESKTOP-8S5DVGP>
2021-11-17 15:32:10 -0800

    pixi uses webgl, webgl requires graphics acceleration, vm's have no graphics acceleration, must dual boot to get a host windows os

--- WebBase/wwwroot/js/site.js
+++ WebBase/wwwroot/js/site.js
@@ -5,10 +5,25 @@
 
 
 function setScreen() {
-
+    //TODO: get something with graphics acceleration, otherwise development on this won't work
+    
     const app = new PIXI.Application({
         width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
     });
+
+    //create a place to store all the things on screen
+    //const containter = new PIXI.containter();
+
+    //container.line(50, 50, 100, 100);
+
+    //const graphics = new PIXI.graphics();
+
+    //graphics.beginFill(0x000000);
+    //graphics.drawRect(50, 50, 100, 100);
+    //graphics.endFill();    
+
+    //const line = new PIXI.line()
+    //select the proper html element and place the pixi object inside of it.
     document.getElementById("availability-table").appendChild(app.view);
-    //document.body.appendChild(app.view);
+    
 }
