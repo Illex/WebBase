e716a118ad287cd45f823af71f9a0bb80c36650f
micron <micron@DESKTOP-SA5J61C>
2021-11-18 11:40:19 -0800

    added pixi boxes to create time grid on page

--- WebBase/wwwroot/js/site.js
+++ WebBase/wwwroot/js/site.js
@@ -8,7 +8,7 @@ function setScreen() {
     //TODO: get something with graphics acceleration, otherwise development on this won't work
     
     const app = new PIXI.Application({
-        width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+        width: 820, height: 740, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
     });
 
     //create a place to store all the things on screen
@@ -16,11 +16,52 @@ function setScreen() {
 
     //container.line(50, 50, 100, 100);
 
-    //const graphics = new PIXI.graphics();
+    const graphics = new PIXI.Graphics();
+    
+    for (i = 0; i < 5; i++) {
+        //top row white out
+        graphics.beginFill(0xFFFFFF);
+        graphics.drawRect(160 * i + 15, 0, 150, 10);
+        graphics.endFill();
 
-    //graphics.beginFill(0x000000);
-    //graphics.drawRect(50, 50, 100, 100);
-    //graphics.endFill();    
+        //bottom row white out
+        graphics.beginFill(0xFFFFFF);
+        graphics.drawRect(160 * i + 15, 735, 150, 10);
+        graphics.endFill();
+        for (j = 0; j < 48; j++) {
+            graphics.beginFill(0x000000);
+            graphics.drawRect(160 * i + 15, 15 * j + 15, 150, 10);
+            graphics.endFill();
+
+            //far left white out
+            graphics.beginFill(0xFFFFFF);
+            graphics.drawRect(0 , 15 * j + 15, 10, 10);
+            graphics.endFill();
+            //far right white out
+            graphics.beginFill(0xFFFFFF);
+            graphics.drawRect(810, 15 * j + 15, 10, 10);
+            graphics.endFill();
+        }        
+    }
+
+    //four corners white out
+    graphics.beginFill(0xFFFFFF);
+    graphics.drawRect(0, 0, 10, 10);
+    graphics.endFill();
+
+    graphics.beginFill(0xFFFFFF);
+    graphics.drawRect(810, 0, 10, 10);
+    graphics.endFill();
+
+    graphics.beginFill(0xFFFFFF);
+    graphics.drawRect(0, 735, 10, 10);
+    graphics.endFill();
+
+    graphics.beginFill(0xFFFFFF);
+    graphics.drawRect(810, 735, 10, 10);
+    graphics.endFill();
+
+    app.stage.addChild(graphics);
 
     //const line = new PIXI.line()
     //select the proper html element and place the pixi object inside of it.
