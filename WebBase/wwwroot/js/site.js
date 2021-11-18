245903a6ac5a224568af997021fed58a12d1c64e
micron <micron@DESKTOP-SA5J61C>
2021-11-18 12:32:47 -0800

    added time labels and highlight bars for every hour

--- WebBase/wwwroot/js/site.js
+++ WebBase/wwwroot/js/site.js
@@ -8,15 +8,16 @@ function setScreen() {
     //TODO: get something with graphics acceleration, otherwise development on this won't work
     
     const app = new PIXI.Application({
-        width: 820, height: 740, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+        width: 900, height: 860, backgroundColor: 0xFFFFFF, resolution: window.devicePixelRatio || 1,
     });
 
     //create a place to store all the things on screen
-    //const containter = new PIXI.containter();
-
-    //container.line(50, 50, 100, 100);
-
     const graphics = new PIXI.Graphics();
+
+    //background color
+    graphics.beginFill(0x1099bb);
+    graphics.drawRect(0, 0, 820, 745);
+    graphics.endFill();
     
     for (i = 0; i < 5; i++) {
         //top row white out
@@ -33,16 +34,29 @@ function setScreen() {
             graphics.drawRect(160 * i + 15, 15 * j + 15, 150, 10);
             graphics.endFill();
 
+            //add a highlight line for each hour
+            if (j % 4 == 0) {
+                graphics.beginFill(0xad0909);
+                graphics.drawRect(160 * i, 15 * j + 10, 180, 5);
+                graphics.endFill();
+            }
+
             //far left white out
             graphics.beginFill(0xFFFFFF);
             graphics.drawRect(0 , 15 * j + 15, 10, 10);
             graphics.endFill();
             //far right white out
             graphics.beginFill(0xFFFFFF);
-            graphics.drawRect(810, 15 * j + 15, 10, 10);
+            graphics.drawRect(810, 15 * j + 15, 820, 10);
             graphics.endFill();
         }        
     }
+
+    //extra highlight line on botton
+    graphics.beginFill(0xad0909);
+    // magic numbers bad
+    graphics.drawRect(0, 730, 820, 5);
+    graphics.endFill();
 
     //four corners white out
     graphics.beginFill(0xFFFFFF);
@@ -87,6 +101,7 @@ function setScreen() {
     Friday.x = 710;
     Friday.y = 0;
 
+
     
     app.stage.addChild(graphics);
     app.stage.addChild(Monday);
@@ -94,6 +109,25 @@ function setScreen() {
     app.stage.addChild(Wednesday);
     app.stage.addChild(Thursday);
     app.stage.addChild(Friday);
+
+    for (i = 0; i <= 12; i++) {
+        t = (i + 8) % 12;
+        if (t == 0) {
+            t = 12
+        }
+
+        temp = 'AM'
+        if (i + 8 >= 12) {
+            temp = 'PM'
+        }
+
+        time = new PIXI.Text(t + ':00 ' + temp, style)
+        time.x = 825;
+        time.y = i * 60 + 6;
+        app.stage.addChild(time);
+    }
+
+    
 
     //const line = new PIXI.line()
     //select the proper html element and place the pixi object inside of it.
