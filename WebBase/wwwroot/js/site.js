3a1d670c140487904875a519f921528ff2ce475d
micron <micron@DESKTOP-SA5J61C>
2021-11-18 11:51:02 -0800

    added day labels to pixi table

--- WebBase/wwwroot/js/site.js
+++ WebBase/wwwroot/js/site.js
@@ -61,7 +61,39 @@ function setScreen() {
     graphics.drawRect(810, 735, 10, 10);
     graphics.endFill();
 
+    //draw text
+    const style = new PIXI.TextStyle({
+        fontFamily: 'Arial',
+        fontSize: 10,       
+        //strokeThickness: 2,
+     });
+    const Monday = new PIXI.Text('Monday',style);
+    Monday.x = 60;
+    Monday.y = 0;
+
+    const Tuesday = new PIXI.Text('Tuesday', style);
+    Tuesday.x = 220;
+    Tuesday.y = 0;
+
+    const Wednesday = new PIXI.Text('Wednesday', style);
+    Wednesday.x = 380;
+    Wednesday.y = 0;
+
+    const Thursday = new PIXI.Text('Thursday', style);
+    Thursday.x = 550;
+    Thursday.y = 0;
+
+    const Friday = new PIXI.Text('Friday', style);
+    Friday.x = 710;
+    Friday.y = 0;
+
+    
     app.stage.addChild(graphics);
+    app.stage.addChild(Monday);
+    app.stage.addChild(Tuesday);
+    app.stage.addChild(Wednesday);
+    app.stage.addChild(Thursday);
+    app.stage.addChild(Friday);
 
     //const line = new PIXI.line()
     //select the proper html element and place the pixi object inside of it.
